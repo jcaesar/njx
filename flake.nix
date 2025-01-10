c6b3152d8a418d354a3db91ec61b40bd99e27784
Julius Michaelis <gitter@liftm.de>
2025-01-10 11:59:40 +0900

    allow nix run

--- flake.nix
+++ flake.nix
@@ -1,9 +1,16 @@
 {
-  outputs = {nixpkgs, ...}: {
-    packages =
-      builtins.mapAttrs (_: pkgs: {
-        default = pkgs.python3.pkgs.callPackage ./. {};
-      })
-      nixpkgs.legacyPackages;
+  outputs = {
+    nixpkgs,
+    self,
+  }: let
+    gen = f: builtins.mapAttrs f nixpkgs.legacyPackages;
+  in {
+    packages = gen (_: pkgs: {
+      default = pkgs.python3.pkgs.callPackage ./. {};
+    });
+    apps = gen (system: _: nixpkgs.lib.genAttrs ["installed" "delete-generations"] (n: {
+      type = "app";
+      program = "${self.packages.${system}.default}/bin/njx-${n}";
+    }));
   };
 }
