f06bc9bdc0560571c8d38dab9df98023f567707e
Julius Michaelis <gitter@liftm.de>
2025-02-21 21:49:32 +0900

    msg to future self

--- mod/protect-boot.nix
+++ mod/protect-boot.nix
@@ -7,6 +7,10 @@
     lib.mkEnableOption "umask=0077 on /boot"
     // {default = true;};
   config = lib.mkIf config.njx.protect-boot {
+    assertions = lib.singleton {
+      assertion = (config.fileSystems."/boot" or {}) ? mountPoint;
+      message = "No /boot, set njx.protect-boot to false";
+    };
     fileSystems."/boot".options = ["umask=0077"];
   };
 }
