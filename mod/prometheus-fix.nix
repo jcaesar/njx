fa3f39aee7be63f3b232337ab91df344dcfcbb0a
Julius Michaelis <gitter@liftm.de>
2025-03-01 09:24:36 +0900

    dreamt up workaround

--- mod/prometheus-fix.nix
+++ mod/prometheus-fix.nix
@@ -1,3 +1,4 @@
+# workaround for https://github.com/prometheus/node_exporter/issues/3261
 {
   config,
   lib,
@@ -6,8 +7,18 @@
   inherit (builtins) any attrValues;
   fs = attrValues config.fileSystems;
   mountNixStore = x: x.mountPoint == "/nix/store";
-  needed = any mountNixStore fs;
+  needed = config.services.prometheus.exporters.node.enable && any mountNixStore fs;
 in {
   services.prometheus.exporters.node.extraFlags =
-    lib.mkIf needed ["--collector.filesystem.mount-points-exclude=/nix/store"];
+    lib.mkIf needed
+    ["--collector.filesystem.mount-points-exclude=/nix/store"];
+  # can't set config.fileSystems here, infinite recursion
+  systemd.mounts = lib.mkIf needed [
+    {
+      what = "/nix/store";
+      where = "/run/.prometheus.export.nix.store";
+      options = "bind";
+      wantedBy = ["prometheus-node-exporter.service"];
+    }
+  ];
 }
