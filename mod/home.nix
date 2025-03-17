5f8a98d42eebad5b4204aaf894b6635ac87aa00f
Julius Michaelis <gitter@liftm.de>
2025-03-17 13:43:30 +0900

    desktop start, still not a solved problem

--- mod/home.nix
+++ mod/home.nix
@@ -137,28 +137,35 @@
   services.swayidle = let
     lock = pkgs.writeShellApplication {
       name = "njx-waylock";
-      runtimeInputs = [nixosConfig.programs.niri.package pkgs.swaylock pkgs.coreutils];
+      runtimeInputs = [nixosConfig.programs.niri.package pkgs.swaylock];
       text = ''
-        timeout --kill-after=15 10 niri msg action power-off-monitors || true
         swaylock_args=(--show-failed-attempts)
         lock_bg="$HOME/.config/swaylock-bg"
         if test -e "$lock_bg"; then
           swaylock_args+=(--image "$lockbg")
         fi
-        exec swaylock "$${swaylock_args[@]}"
+        exec swaylock "''${swaylock_args[@]}"
       '';
     };
   in
     lib.mkIf nixosConfig.programs.niri.enable {
       enable = true;
-      events = lib.singleton {
-        event = "before-sleep";
-        command = lib.getExe lock;
-      };
-      timeouts = lib.singleton {
-        timeout = 310;
-        command = lib.getExe lock;
-      };
+      events = [
+        {
+          event = "before-sleep";
+          command = lib.getExe lock;
+        }
+      ];
+      timeouts = [
+        {
+          timeout = 300;
+          command = "niri msg action power-off-monitors";
+        }
+        {
+          timeout = 310;
+          command = lib.getExe lock;
+        }
+      ];
     };
   systemd.user.services.swww = lib.mkIf nixosConfig.programs.niri.enable {
     Service.ExecStart = lib.getExe' pkgs.swww "swww-daemon";
