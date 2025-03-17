f66aa4407bc88925e00e8f12dac4c7339f1189d7
Julius Michaelis <gitter@liftm.de>
2025-03-17 22:07:00 +0900

    cargo cult

--- mod/bluetooth.nix
+++ mod/bluetooth.nix
@@ -10,4 +10,12 @@
       Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
     };
   };
+  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+    "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+    };
+  };
 }
