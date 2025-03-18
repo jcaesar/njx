0a76571a0c4d183a7e65d1ef5c65372e12f9905e
Julius Michaelis <gitter@liftm.de>
2025-03-18 21:24:15 +0900

    modded till broke

--- mod/bluetooth.nix
+++ mod/bluetooth.nix
@@ -10,12 +10,4 @@
       Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
     };
   };
-  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-    "monitor.bluez.properties" = {
-      "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = true;
-      "bluez5.enable-hw-volume" = true;
-      "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
-    };
-  };
 }
