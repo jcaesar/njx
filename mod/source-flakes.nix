1557d7c1e894c57f074df29f21a8cc5ff458c5f5
Julius Michaelis <gitter@liftm.de>
2025-03-20 22:17:59 +0900

    flake n everywhere

--- mod/source-flakes.nix
+++ mod/source-flakes.nix
@@ -5,15 +5,29 @@
   ...
 }: {
   options.njx.source-flakes = lib.mkEnableOption "flake sources in /etc";
-  config = lib.mkIf config.njx.source-flakes {
-    environment.etc = let
-      mkLnk = name: flake: {
-        name = "sysflake/${name}";
-        value.source = flake;
-      };
-    in
-      lib.mapAttrs' mkLnk flakes;
-    nix.registry.n.flake = flakes.nixpkgs;
-    nix.registry.sf.flake = flakes.self;
-  };
+  config = lib.mkMerge [
+    (
+      lib.mkIf config.njx.source-flakes {
+        environment.etc = let
+          mkLnk = name: flake: {
+            name = "sysflake/${name}";
+            value.source = flake;
+          };
+        in
+          lib.mapAttrs' mkLnk flakes;
+        nix.registry.n.flake = flakes.nixpkgs;
+        nix.registry.sf.flake = flakes.self;
+      }
+    )
+    (
+      lib.mkIf (!config.njx.source-flakes) {
+        nix.registry.n.to = {
+          type = "github";
+          owner = "NixOS";
+          repo = "nixpkgs";
+          rev = flakes.nixpkgs.rev;
+        };
+      }
+    )
+  ];
 }
