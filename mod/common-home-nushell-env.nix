28296505555ffc0a6c7bfe621ed467a8a427a566
Julius Michaelis <gitter@liftm.de>
2025-03-27 21:57:01 +0900

    hackstack

--- mod/common-home-nushell-env.nix
+++ mod/common-home-nushell-env.nix
@@ -8,7 +8,18 @@
   # can't be in home.nix because that doesn't have access to config(?), can't be in common.nix because of the key collision
   home-manager = lib.mkIf config.njx.common {
     users.julius.programs.nushell.extraEnv = ''
-      open ${pkgs.writeText "environment.variables.json" (builtins.toJSON config.environment.variables)} | load-env
+      mut cev = open ${pkgs.writeText "environment.variables.json" (builtins.toJSON config.environment.variables)}
+      for var in [XCURSOR_PATH XDG_CONFIG_DIRS XDG_DATA_DIRS PATH] {
+        if $var in $cev and $var in $env {
+          let merge = [$cev $env]
+            | each { get $var | split row ":" }
+            | flatten
+            | uniq
+            | str join ":"
+          $cev = $cev | update $var $merge
+        }
+      }
+      $cev | load-env
     '';
   };
 }
