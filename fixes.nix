7a2819025993e9369a940ce5b32f105742fb6569
Julius Michaelis <gitter@liftm.de>
2025-05-06 13:43:23 +0900

    flake lock

--- fixes.nix
+++ fixes.nix
@@ -7,4 +7,9 @@ final: prev: {
       };
     };
   };
+  libselinux = prev.libselinux.overrideAttrs (orig: if final.stdenv.hostPlatform.isStatic then {
+    patches =
+      final.lib.singleton "${builtins.fetchTree "github:NixOS/nixpkgs/a1d539f8eecffd258f3ed1ccc3a055aed56fbaa1"}/pkgs/by-name/li/libselinux/fix-build-32bit-lfs.patch"
+      ++ final.lib.filter (x: builtins.match ".*fix-build-32bit-lfs.patch" "${x}" == null) orig.patches;
+  } else {});
 }
