d1d3e9c5f223664cd465de523f96ce11a63d3172
Julius Michaelis <gitter@liftm.de>
2025-05-23 13:20:44 +0900

    nicht so fix

--- fixes.nix
+++ fixes.nix
@@ -7,19 +7,4 @@ final: prev: {
       };
     };
   };
-  libselinux = prev.libselinux.overrideAttrs (orig:
-    if final.stdenv.hostPlatform.isStatic
-    then {
-      patches =
-        final.lib.singleton "${builtins.fetchTree "github:NixOS/nixpkgs/a1d539f8eecffd258f3ed1ccc3a055aed56fbaa1"}/pkgs/by-name/li/libselinux/fix-build-32bit-lfs.patch"
-        ++ final.lib.filter (x: builtins.match ".*fix-build-32bit-lfs.patch" "${x}" == null) orig.patches;
-    }
-    else {});
-  brscan5 = prev.brscan5.overrideAttrs (orig: {
-    postPatch = ''
-      ${orig.postPatch}
-      # SYSFS removed with udev-173, see pkgs/by-name/br/brscan4/udev_rules_type1.nix
-      sed -i -e s/SYSFS/ATTR/g opt/brother/scanner/brscan5/udev-rules/*.rules
-    '';
-  });
 }
