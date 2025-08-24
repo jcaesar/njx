503640f6a918108eb6b0085aeba9a90d732a4906
Julius Michaelis <gitter@liftm.de>
2025-08-24 22:27:59 +0900

    bake other half

--- sys/westie.nix
+++ sys/westie.nix
@@ -1,10 +1,13 @@
-# an sftp fileserver - likely one of the lamest configs i have
 {
   networking.hostName = "westie";
   boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "usbhid" "sr_mod"];
+  boot.initrd.systemd.enable = true;
   hardware.cpu.intel.updateMicrocode = true;
   njx.base = true;
+  services.sshd.enable = true;
   users.users.root.openssh.authorizedKeys.keys = (import ../private.nix).terminalKeys;
+  njx.sshUnlock.keys = (import ../private.nix).terminalKeys;
+  njx.sshUnlock.modules = ["tg3" "e1000e"];
   fileSystems."/" = {
     fsType = "btrfs";
     device = "/dev/mapper/nixroot";
@@ -24,6 +27,28 @@
   };
   boot.initrd.luks.devices.data = {
     device = "/dev/disk/by-label/data";
+  };
+  njx.manual.disks = ''
+    ```
+    sgdisk --clear --align-end --new=1:0:+1G --typecode=1:EF00 --change-name=1:nixboot --new=2:0:-0 --typecode=2:8300 /dev/nvme0n1
+    cryptsetup luksFormat /dev/nvme0n1p2 --label nixcrypt
+    cryptsetup luksOpen /dev/disk/by-label/nixcrypt nixroot
+    mkfs.btrfs /dev/mapper/nixroot
+    mount /dev/mapper/nixroot /mnt -odiscard,compress=zstd
+    mkfs.fat -F32 /dev/disk/by-partlabel/nixboot
+    mkdir /mnt/boot
+    mount /dev/disk/by-partlabel/nixboot /mnt/boot/
+    cryptsetup luksFormat /dev/disk/by-id/ata-TOSHIBA_MG10AFA22TE_X4B0A028FM8J --label data
+    ```
+    - The label on the ata-TOSHIBA LUKS didn't stick
+    - May need manual bootctl install after nixos-install.
+  '';
+  systemd.network = {
+    enable = true;
+    networks.whatev = {
+      matchConfig.Name = "enp*";
+      DHCP = "yes";
+    };
   };
   system.stateVersion = "25.11";
 }
