65d61315a9be4f2012e94a4433d8bb96fc1808d5
Julius Michaelis <gitter@liftm.de>
2025-10-24 15:42:38 +0900

    ifd ied

--- mod/extra-initrd.nix
+++ mod/extra-initrd.nix
@@ -3,18 +3,19 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  cfg = config.njx.extraInitrdClosures;
+  set = lib.pipe cfg [
+    builtins.toJSON
+    (pkgs.writeText "supplicant-wlan0-service-config.json")
+    pkgs.writeClosure
+    lib.readFile
+    (lib.removeSuffix "\n")
+    (lib.splitString "\n")
+  ];
+in {
   options.njx.extraInitrdClosures = lib.mkOption {
     default = [];
   };
-  config.boot.initrd.systemd.storePaths =
-    lib.pipe config.njx.extraInitrdClosures
-    [
-      builtins.toJSON
-      (pkgs.writeText "supplicant-wlan0-service-config.json")
-      pkgs.writeClosure
-      lib.readFile
-      (lib.removeSuffix "\n")
-      (lib.splitString "\n")
-    ];
+  config.boot.initrd.systemd.storePaths = lib.mkIf (cfg != []) set;
 }
