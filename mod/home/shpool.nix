efbd798b9bcf14dcc56d5742b749f539e3e4c533
Julius Michaelis <gitter@liftm.de>
2025-11-19 21:32:09 +0900

    winded

--- mod/home/shpool.nix
+++ mod/home/shpool.nix
@@ -10,9 +10,7 @@
   tomlFormat = pkgs.formats.toml {};
 in {
   options.services.shpool = {
-    enable =
-      lib.mkEnableOption description
-      // {default = !(nixosConfig.njx.graphical or true);};
+    enable = lib.mkEnableOption description;
     package = lib.mkPackageOption pkgs "shpool" {};
     settings = lib.mkOption {
       type = tomlFormat.type;
@@ -21,37 +19,46 @@ in {
         Configuration written to
         {file}`$XDG_CONFIG_HOME/shpool/config.yml`.
 
-        See https://github.com/shell-pool/shpool/blob/master/CONFIG.md
+        See https://github.com/shell-pool/shpool/blob/eff45bf58bd63fa60ea7f4d055b1116b4a475c1f/libshpool/src/config.rs#L180 and https://github.com/shell-pool/shpool/blob/master/CONFIG.md
       '';
     };
   };
-  # https://github.com/shell-pool/shpool/tree/eff45bf58bd63fa60ea7f4d055b1116b4a475c1f/systemd
-  config = lib.mkIf cfg.enable {
-    services.shpool.settings.prompt_prefix =
-      lib.mkIf config.programs.nushell.enable "";
-    xdg.configFile."shpool/config.toml".source = tomlFormat.generate "shpool-config.toml" cfg.settings;
-    home.packages = [cfg.package];
-    systemd.user.services.shpool = {
-      Unit = {
-        Description = description;
-        Requires = ["shpool.socket"];
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      xdg.configFile."shpool/config.toml".source = tomlFormat.generate "shpool-config.toml" cfg.settings;
+      home.packages = [cfg.package];
+      # https://github.com/shell-pool/shpool/tree/eff45bf58bd63fa60ea7f4d055b1116b4a475c1f/systemd
+      systemd.user.services.shpool = {
+        Unit = {
+          Description = description;
+          Requires = ["shpool.socket"];
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${lib.getExe cfg.package} daemon";
+          KillMode = "mixed";
+          TimeoutStopSec = "2s";
+          SendSIGHUP = "yes";
+        };
+        # Install.WantedBy = ["defaults.target"]; # upstream sets. but there's the socket…
       };
-      Service = {
-        Type = "simple";
-        ExecStart = "${lib.getExe cfg.package} daemon";
-        KillMode = "mixed";
-        TimeoutStopSec = "2s";
-        SendSIGHUP = "yes";
+      systemd.user.sockets.shpool = {
+        Unit.Description = description;
+        Install.WantedBy = ["sockets.target"];
+        Socket = {
+          ListenStream = "%t/shpool/shpool.socket";
+          SocketMode = "0600";
+        };
       };
-      # Install.WantedBy = ["defaults.target"]; # upstream sets. but there's the socket…
-    };
-    systemd.user.sockets.shpool = {
-      Unit.Description = description;
-      Install.WantedBy = ["sockets.target"];
-      Socket = {
-        ListenStream = "%t/shpool/shpool.socket";
-        SocketMode = "0600";
-      };
-    };
-  };
+    })
+    {
+      services.shpool.settings = lib.mkIf config.programs.nushell.enable (lib.mkDefault {
+        prompt_prefix = "";
+        forward_env = ["PATH"];
+      });
+      services.shpool.enable =
+        (nixosConfig.njx.common or false)
+        && !(nixosConfig.njx.graphical or true);
+    }
+  ];
 }
