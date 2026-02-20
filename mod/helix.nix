b95205cb481181fcc9701b330e9b739e511d3271
Julius Michaelis <gitter@liftm.de>
2026-02-20 11:32:06 +0900

    halix

--- mod/helix.nix
+++ mod/helix.nix
@@ -14,28 +14,36 @@ in {
     njx.helix.noLocalLangs = lib.mkDefault (cfg.slim || config.nixpkgs.system == "x86_64-linux");
     nixpkgs.overlays = lib.mkIf (cfg.slim || cfg.noLocalLangs) [
       (final: prev: {
-        helix = prev.helix.overrideAttrs (old: {
-          patches =
-            (final.patches or [])
-            ++ pkgs.lib.optional cfg.noLocalLangs ../pkgs/helix.patch;
-          postInstall =
-            old.postInstall or ""
-            + lib.optionalString cfg.slim
-            ''
-              find $out/lib/runtime/grammars/ \
-                -name '*.so' \
-                -type f \
-                ! -name nu.so \
-                ! -name bash.so \
-                ! -name python.so \
-                ! -name nginx.so \
-                ! -name yaml.so \
-                ! -name json.so \
-                ! -name nix.so \
-                -delete
-              test $(du -ks $out | grep -oE '^[0-9]*') -lt 50000
-            '';
-        });
+        helix =
+          lib.pipe prev.helix
+          [
+            (x:
+              x.overrideAttrs (old: {
+                patches =
+                  (final.patches or [])
+                  ++ pkgs.lib.optional cfg.noLocalLangs ../pkgs/helix.patch;
+              }))
+            (x:
+              x.override {
+                lockedGrammars =
+                  lib.pipe "${lib.dirOf x.meta.position}/grammars.json"
+                  [
+                    lib.importJSON
+                    (lib.filterAttrs (
+                      k: _:
+                        lib.elem k [
+                          "nu"
+                          "bash"
+                          "python"
+                          "nginx"
+                          "yaml"
+                          "json"
+                          "nix"
+                        ]
+                    ))
+                  ];
+              })
+          ];
       })
     ];
   };
