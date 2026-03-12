8da8ca32f5617e96adaf1520ba10ecf5c6f5c814
Julius Michaelis <michaelis@fujitsu.com>
2026-03-12 10:13:49 +0900

    es saugt

--- pkgs/nushell-plugin-plotters/package.nix
+++ pkgs/nushell-plugin-plotters/package.nix
@@ -4,16 +4,25 @@
   fetchCrate,
   fontconfig,
   pkg-config,
+  nushell,
 }:
+  let origNu = "0.110.0"; in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "nu_plugin_plotters";
-  version = "0.2.4+0.110.0";
+  version = "0.2.4+${origNu}";
 
   src = fetchCrate {
     inherit (finalAttrs) version pname;
     hash = "sha256-fddkK468X+bkSklZXwv83/ZlT/rf2l8LhD8s9mTEHTA=";
   };
-  cargoHash = "sha256-x6+YKx7TfQfJg2JB03UMyPGgpVQaPtofGXtxb6QNlr4=";
+  cargoLock.lockFile = {
+    "${origNu}" = "${finalAttrs.src}/Cargo.lock";
+    "0.111.0" = ./lock-0.111.0.toml;
+    }.${nushell.version} or (throw "Need to prepare lock file for nushell version ${nushell.version}");
+  prePatch = ''
+    sed -ri 's/"${origNu}"/"*"/' Cargo.toml
+    cp ../cargo-vendor-dir/Cargo.lock Cargo.lock
+  '';
 
   buildInputs = [fontconfig];
   nativeBuildInputs = [pkg-config];
