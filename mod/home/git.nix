215db4009febc41275c3ae61cf906b509df5e487
Julius Michaelis <gitter@liftm.de>
2026-04-22 09:34:47 +0900

    git to da choppa

--- mod/home/git.nix
+++ mod/home/git.nix
@@ -13,7 +13,8 @@ in {
       alias = {
         l = "log --oneline --decorate --all --graph";
         lg = "log --graph --abbrev-commit --decorate --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
-        quickserve = "daemon --verbose --export-all --base-path=.git --reuseaddr --strict-paths .git/";
+        quickserve = "daemon --verbose --export-all --base-path=.git --reuseaddr --strict-paths.git/";
+        out = "log @{u}..";
       };
       pull.ff = "only";
       rerere.enable = true;
@@ -21,6 +22,40 @@ in {
       delta.nagivate = true; # use n and N to move between diff sections
       "credential \"https://github.com\"".helper = creds;
       "credential \"https://gist.github.com\"".helper = creds;
+      advice.detachedHead = false;
+      # cargo culting https://blog.gitbutler.com/how-git-core-devs-configure-git
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "default";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      merge.conflictstyle = "zdiff3";
     };
   };
   programs.difftastic.enable = true;
