78061905b59f922af90e1cede9cffc880d66a0d7
Julius Michaelis <gitter@liftm.de>
2026-05-25 11:25:33 +0900

    history written by the vector

--- pkgs/vector-cloudwatchsyslogs.nix
+++ pkgs/vector-cloudwatchsyslogs.nix
@@ -3,6 +3,8 @@ pkgs.vector.overrideAttrs {
   cargoBuildFeatures = [
     "unix"
     "sinks-aws_cloudwatch_logs"
+    "sources-vector"
+    "sinks-vector"
     "sources-syslog"
     "sources-journald"
     "transforms-filter"
