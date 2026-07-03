68c2985af51a538a350242393b402bf40d6ee414
Julius Michaelis <gitter@liftm.de>
2026-07-04 08:34:00 +0900

    too leaky

--- mod/leakrets.nix
+++ mod/leakrets.nix
@@ -10,6 +10,12 @@ in {
     what = bootFs.device;
     where = "/boot";
     type = bootFs.fsType;
+    requiredBy = ["sshd.service"];
+    before = ["sshd.service"];
+    mountConfig = lib.mkIf (config.njx.protect-boot) {
+      # You'd think this doesn't matter in initrd, but sshd is a stickler
+      Options= "umask=0077";
+    };
   };
   boot.initrd.network.ssh.hostKeys = lib.mkForce [
     "/boot/leakrets/ssh/host_rsa_key"
