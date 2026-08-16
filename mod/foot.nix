0b6528dfce409880031823dc2857b8e5a92fd0b2
Julius Michaelis <gitter@liftm.de>
2026-08-16 13:02:02 +0900

    dot prod

--- mod/foot.nix
+++ mod/foot.nix
@@ -5,6 +5,7 @@
       main = {
         term = "xterm-256color";
         font = "DejaVuSansMono:size=6.5";
+        # doesn't do anything, thus I keep a seperate version of this file for each machine/display :(
         dpi-aware = "yes";
       };
       colors-dark.alpha = 0.8;
