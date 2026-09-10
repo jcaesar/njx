1502e12b2020edb2eb479647b2b16b8f1926e985
Julius Michaelis <gitter@liftm.de>
2026-09-10 10:54:31 +0900

    wip

--- pkgs/nu_plugin_dots.nix
+++ pkgs/nu_plugin_dots.nix
@@ -17,15 +17,22 @@ buildPythonApplication (finalAttrs: {
     install -Dm555 ${../mod/home/nu/nu_plugin_dots.py} $out/bin/${finalAttrs.pname}
   '';
   doCheck = true;
-  checkPhase = ''
-    mkdir -p "$NIX_BUILD_TOP/nucfg"
-    export XDG_CONFIG_HOME="$NIX_BUILD_TOP/nucfg"
-    nu -c '
-      plugin add ($env.out)/bin/nu_plugin_dots
-      plugin use dots
-      let pts = [[lon lat]; [1 2] [3 4] [5 6]]
-      let svg = $pts | dots --no-basemap
-      assert ($svg | str contains "<svg")
-    ' 
-  '';
+   checkPhase = ''
+     mkdir -p "$NIX_BUILD_TOP/nucfg"
+     export XDG_CONFIG_HOME="$NIX_BUILD_TOP/nucfg"
+     export XDG_DATA_HOME="$NIX_BUILD_TOP/ndata"
+     mkdir -p "$NIX_BUILD_TOP/ndata"
+     echo "HOME=[$HOME] XDG_CONFIG_HOME=[$XDG_CONFIG_HOME] NIX_BUILD_TOP=[$NIX_BUILD_TOP]"
+     nu -c 'plugin add ($env.out)/bin/nu_plugin_dots' || echo "ADD FAILED $?"
+     echo "--- files after add ---"
+     find "$NIX_BUILD_TOP" -type f 2>/dev/null
+     nu -c 'plugin use dots' 2>&1 | cat; echo "USE_EXIT=$?"
+     nu -c '
+       plugin add ($env.out)/bin/nu_plugin_dots
+       plugin use dots
+       let pts = [[lon lat]; [1 2] [3 4] [5 6]]
+       let svg = $pts | dots --no-basemap
+       assert ($svg | str contains "<svg")
+     '
+   '';
 })
