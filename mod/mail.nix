66a7ed825efa026dea92032b7188edeed2244318
Julius Michaelis <gitter@liftm.de>
2026-09-16 08:41:18 +0900

    hast du brot? oder kamel?

--- mod/mail.nix
+++ mod/mail.nix
@@ -1,6 +1,10 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   users.users.julius.packages = with pkgs; [
-    # (himalaya.override {buildFeatures = ["notmuch"];})
+    # (himalaya.override {buildFeatures = ["maildir"];}) # dropped notmuch support and maildir can't handle 20k emails in the inbox
     (writeScriptBin "notmuch" ''
       #!${runtimeShell}
       export XAPIAN_CJK_NGRAM=1
@@ -21,4 +25,20 @@
       * `/home/julius/.notmuch-config-sample`
       * `/home/julius/.offlineimaprc-sample`
   '';
+  # https://github.com/RaitoBezarius/nixos-home/blob/main/emails/neomutt.nix
+  home-manager.users.julius = {
+    programs.neomutt = {
+      enable = true;
+      sidebar.width = 40;
+      sidebar.enable = true;
+      sidebar.shortPath = true;
+      sidebar.format = "%D%> %?N?%N/?%S";
+      vimKeys = true;
+      sort = "reverse-date";
+      extraConfig = ''
+        set nm_default_url = "notmuch:///home/foo/maildir"
+        named-mailboxes "My INBOX" "notmuch://?query=tag:inbox"
+      '';
+    };
+  };
 }
